4ba8d762c1328a95152634e2f4d062495f041eeb
belyanin <belyanin@skbkontur.ru>
2021-03-27 11:11:22 +0500

    Alpha version

--- Qryptoimage.App/MainWindow.xaml.cs
+++ Qryptoimage.App/MainWindow.xaml.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using Microsoft.Win32;
-using Brushes = System.Windows.Media.Brushes;
 using Image = System.Drawing.Image;
 
 namespace Qryptoimage.App
@@ -50,27 +49,41 @@ namespace Qryptoimage.App
 
         private void Decrypt(object sender, RoutedEventArgs routedEventArgs)
         {
+            if (string.IsNullOrWhiteSpace(KeyInput.Text))
+            {
+                TextInput.Text = LSB.Decode(bitmap);
+                
+                StatusLabel.Content = "Decrypted without key";
+            }
+            else if (Guid.TryParse(KeyInput.Text.Trim(), out var guid))
+            {
+                var text = LSB.Decode(bitmap);
 
-            var x = LSB.Decode(bitmap);
-            TextInput.Text = x;
-            //var text = Crypter.Encrypt("hello", Guid.Parse("7ab9bc21-227c-4207-86bc-32f6e95388fe"));
-
-            //TextInput.Text = Crypter.Decrypt(text, Guid.Parse("7ab9bc21-227c-4207-86bc-32f6e95388fe")); //7ab9bc21-227c-4207-86bc-32f6e95388fe
+                TextInput.Text = Crypter.Decrypt(text, guid);
+                
+                StatusLabel.Content = "Decrypted with given key";
+            }
+            else
+            {
+                StatusLabel.Content = "Invalid key";
+            }
         }
 
         private void GenerateKey(object sender, RoutedEventArgs e)
         {
             KeyInput.Text = Guid.NewGuid().ToString();
         }
-        
-        public BitmapImage Convert(Bitmap src)
+
+        private static BitmapImage Convert(Image src)
         {
-            MemoryStream ms = new MemoryStream();
-            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
-            BitmapImage image = new BitmapImage();
+            var memoryStream = new MemoryStream();
+            src.Save(memoryStream, ImageFormat.Bmp);
+            var image = new BitmapImage();
+            
             image.BeginInit();
-            ms.Seek(0, SeekOrigin.Begin);
-            image.StreamSource = ms;
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = memoryStream;
+            
             image.EndInit();
             return image;
         }
@@ -85,7 +98,7 @@ namespace Qryptoimage.App
             }
             else if (Guid.TryParse(KeyInput.Text.Trim(), out var guid))
             {
-                var text = Crypter.Encrypt(KeyInput.Text, guid);
+                var text = Crypter.Encrypt(TextInput.Text, guid);
                 
                 Encode(text);
                 
